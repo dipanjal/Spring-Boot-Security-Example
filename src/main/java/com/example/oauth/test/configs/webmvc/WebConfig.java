3a0d6c5c085e169d7c3a4fbc7b4537e2b271364d
dipanjal <dipanjalmaitra@gmail.com>
2019-07-17 16:30:49 +0600

    resource handler added

---
+++ src/main/java/com/example/oauth/test/configs/webmvc/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.oauth.test.configs.webmvc;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/WEB-INF/resources/");
+    }
+
+
+}
